41a38926d0678adadadec33767e57036e728f460
setare <ssaedi1995@gmail.com>
2024-07-23 10:59:55 +0330

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Timeline from './sections/main/Timeline';
+import Header from './sections/Header';
+import About from './sections/main/About';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="  font-yakan py-12  mx-auto">
+     
+     <Header/>
+     
+      <div className="max-w-[986px] mx-auto shadow-md rounded-md pb-16">
+       
+       <Timeline/>
+
+       <About />
+
+      </div>
+
     </div>
   );
 }
---
+++ src/sections/main/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <div className='flex flex-row justify-between items-end mx-20 relative'>
+          <div className='bg-[#FFF1ED] w-fit text-center rounded-2xl border-solid border-4 border-white absolute top-0 drop-shadow-xl '>
+            <div className='font-bold text-3xl py-2 '>24</div>
+            <div className='pb-1 bg-[#FF6437] text-white rounded-2xl text-sm px-3 border-solid ring-4 ring-white m-0'>فروردین</div>
+          </div>
+
+          <div className='bg-[#F0F8FF] relative border-4 border-white rounded-2xl px-8 pb-6 ml-16 shadow-xl pt-16 '>
+            <div className='text-[115px]  absolute bottom-[40%] text-[#878F9B]'>,,</div>
+
+            <div className='text-xs my-4 text-[#424244]'>
+              با استفاده از سرویس گارد ژاکت قادر خواهید بود برای محصولات خود در ژاکِت لایسنس تعریف کرده و از نصب‌های متعدد، کپی و انتشار نسخه نال
+            </div>
+            <div className='font-bold text-[#878F9B] text-xs'>علیرضا محمدی فروشنده ژاکت</div>
+          </div>
+          <div>
+            <img src='./img/1.jpg' alt='woodmart' className=' rounded-2xl shadow-xl' />
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
